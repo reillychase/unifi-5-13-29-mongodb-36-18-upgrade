62ecc566c3455fee8ce604f86bb2700a9553a21c
Reilly Chase <rchase@rchase.com>
2020-06-18 16:54:36 -0400

    Create downgrade.sh

---
+++ downgrade.sh
@@ -0,0 +1,21 @@
+apt-get autoremove -y
+apt-get autoclean -y
+wget -O /root/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
+dpkg -i /root/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
+
+wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update -y
+apt-get install -y mongodb-org
+apt-get install -y mongodb-org=3.4.23 mongodb-org-server=3.4.23 mongodb-org-shell=3.4.23 mongodb-org-mongos=3.4.23 mongodb-org-tools=3.4.23 --allow-downgrades
+
+dpkg -i --force-overwrite /var/cache/apt/archives/mongodb-org-tools_3.4.24_amd64.deb
+apt-get install -f -y --allow-unauthenticated
+
+service unifi stop
+service unifi start
+
+mongo --port 27117 --eval 'db.adminCommand( { setFeatureCompatibilityVersion: "3.4" } )'
+
+wget -O unifi_sysvinit_all.deb https://dl.ui.com/unifi/5.12.72/unifi_sysvinit_all.deb
+dpkg -i unifi_sysvinit_all.deb
